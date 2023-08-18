528a4f00efc2d752dbd3b23735c0fa45721979d9
Just-Naf <justivanelafif@gmail.com>
2023-08-18 10:36:04 +0700

    setia tapi bukan yang pertama

--- tarik.php
+++ tarik.php
@@ -1,6 +1,11 @@
 <?php
 
-include "koneksi.php";
+include("koneksi.php");
+
+session_start();
+$id_users = $_SESSION['id_users'];
+$username = $_SESSION['username'];
+
 
 if( isset($_GET['id_setor']) ){
 
@@ -8,13 +13,13 @@ if( isset($_GET['id_setor']) ){
     $id_setor = $_GET['id_setor'];
 
     // buat query hapus
-    $sql = "UPDATE setor SET flag ='1' WHERE flag='0' AND setor.id_users = 1;";
+    $sql = "UPDATE setor SET flag_tarik ='1' WHERE flag_tarik='0' AND setor.id_users='$_SESSION[id_users]';";
     $query = mysqli_query($koneksi, $sql);
 
     // apakah query hapus berhasil?
     if( $query ){
-        header('Location: dasbor.php');
-        die("selamat saldo anda berhasil di tarik");
+        echo "<script>alert('Data telah tersimpan')</script>";
+        header('Location: saldo.php');
     } else {
         die("gagal tarik...");
     }
